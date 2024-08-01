cc7d8cf17982c86dd7507e71a823809324c6dce1
Jakub Zohar Rutkowski <kuba@skb.pl>
2024-08-01 10:09:57 +0200

    Platform subscription plans extended

--- ITBees.FAS.Payments/Interfaces/Models/PlatformFeature.cs
+++ ITBees.FAS.Payments/Interfaces/Models/PlatformFeature.cs
@@ -6,4 +6,6 @@ public class PlatformFeature
     public string FeatureName { get; set; }
     public string? FeatureDescription { get; set; }
     public bool IsVisible { get; set; }
+    public string GroupName { get; set; }
+    public string FeatureValueDescription { get; set; }
 }
