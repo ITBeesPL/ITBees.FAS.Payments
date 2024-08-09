235adce97008396707a1201cbecb2fba70e8b785
Jakub Zohar Rutkowski <kuba@skb.pl>
2024-08-09 20:43:22 +0200

    Translation discovery

---
+++ ITBees.FAS.Payments/Translations/CurrentAssembly.cs
@@ -0,0 +1,12 @@
+﻿using ITBees.Translations.Interfaces;
+using ITBees.UserManager.Translations;
+
+namespace ITBees.FAS.Payments.Translations;
+
+public class CurrentAssembly
+{
+    public static List<ITranslate> GetTranslationClasses()
+    {
+        return [new ApplySubscriptionPlan()];
+    }
+}
