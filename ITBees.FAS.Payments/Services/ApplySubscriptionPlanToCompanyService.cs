ce4cda136f4181cd06a5fb709979cfac99f91d08
Jakub Zohar Rutkowski <kuba@skb.pl>
2024-08-09 20:40:07 +0200

    apply subscription plan for user that already used trial once again will return translated error

--- ITBees.FAS.Payments/Services/ApplySubscriptionPlanToCompanyService.cs
+++ ITBees.FAS.Payments/Services/ApplySubscriptionPlanToCompanyService.cs
@@ -2,6 +2,9 @@
 using ITBees.FAS.Payments.Interfaces.Models;
 using ITBees.Interfaces.Repository;
 using ITBees.Models.Companies;
+using ITBees.Models.Languages;
+using ITBees.Translations;
+using ITBees.UserManager.Interfaces.Services;
 using Microsoft.Extensions.Logging;
 
 namespace ITBees.FAS.Payments.Services;
@@ -10,15 +13,33 @@ class ApplySubscriptionPlanToCompanyService : IApplySubscriptionPlanToCompanySer
 {
     private readonly IWriteOnlyRepository<Company> _companyRwRepo;
     private readonly ILogger<ApplySubscriptionPlanToCompanyService> _logger;
+    private readonly IReadOnlyRepository<Company> _companyRoRepo;
+    private readonly IAspCurrentUserService _aspCurrentUserService;
 
-    public ApplySubscriptionPlanToCompanyService(IWriteOnlyRepository<Company> companyRwRepo, ILogger<ApplySubscriptionPlanToCompanyService> logger)
+    public ApplySubscriptionPlanToCompanyService(
+        IWriteOnlyRepository<Company> companyRwRepo,
+        ILogger<ApplySubscriptionPlanToCompanyService> logger,
+        IReadOnlyRepository<Company> companyRoRepo,
+        IAspCurrentUserService aspCurrentUserService)
     {
         _companyRwRepo = companyRwRepo;
         _logger = logger;
+        _companyRoRepo = companyRoRepo;
+        _aspCurrentUserService = aspCurrentUserService;
     }
 
     public void Apply(PlatformSubscriptionPlan subscriptionPlan, Guid companyGuid)
     {
+
+        if (subscriptionPlan.IsTrial)
+        {
+            var company = _companyRoRepo.GetFirst(x => x.Guid == companyGuid);
+            if (company.CompanyPlatformSubscription.TrialNotAvailable)
+            {
+                var language = _aspCurrentUserService.GetCurrentUser() == null ? new En() : _aspCurrentUserService.GetCurrentUser().Language;
+                throw new ArgumentException(Translate.Get(() => Translations.ApplySubscriptionPlan.Errors.TrialPlanAlreadyUsed, language));
+            }
+        }
         _companyRwRepo.UpdateData(x => x.Guid == companyGuid, x =>
         {
             if (subscriptionPlan.IsTrial)
---
+++ ITBees.FAS.Payments/Translations/ApplySubscriptionPlan.cs
@@ -0,0 +1,11 @@
+﻿using ITBees.Translations.Interfaces;
+
+namespace ITBees.FAS.Payments.Translations;
+
+public class ApplySubscriptionPlan : ITranslate
+{
+    public class Errors : ITranslate
+    {
+        public static readonly string TrialPlanAlreadyUsed = "";
+    }
+}
