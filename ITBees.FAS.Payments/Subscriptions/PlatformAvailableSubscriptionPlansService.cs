9546afd5320f20fdbc01856fa8a060786a8ae6c9
Jakub Zohar Rutkowski <kuba@skb.pl>
2024-09-24 09:29:37 +0200

    Order platform subscription plans by position

--- ITBees.FAS.Payments/Subscriptions/PlatformAvailableSubscriptionPlansService.cs
+++ ITBees.FAS.Payments/Subscriptions/PlatformAvailableSubscriptionPlansService.cs
@@ -53,7 +53,11 @@ class PlatformAvailableSubscriptionPlansService : IPlatformAvailableSubscription
     {
         var result = _platformSubscriptionPlanRoPlan
             .GetDataQueryable(x => x.IsActive, 
-                x=>x.PlanFeatures, x=>x.Language).Include(x=>x.PlanFeatures).ThenInclude(x=>x.PlatformFeature).ToList();
+                x=>x.PlanFeatures, x=>x.Language)
+            .Include(x=>x.PlanFeatures)
+            .ThenInclude(x=>x.PlatformFeature)
+            .OrderBy(x=>x.Position)
+            .ToList();
 
         return result.Select(x=>new PlatformSubscriptionPlanVm(x)).ToList();
     }
