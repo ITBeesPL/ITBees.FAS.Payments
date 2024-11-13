98c575dc1ad7c563ae7f4bc54016d142ee2b390e
Jakub Zohar Rutkowski <kuba@skb.pl>
2024-11-13 16:05:54 +0100

    Payment list, payment logs exposed

---
+++ ITBees.FAS.Payments/Controllers/Models/PaymentLogVm.cs
@@ -0,0 +1,26 @@
+﻿using ITBees.FAS.Payments.Interfaces.Models;
+
+namespace ITBees.FAS.Payments.Controllers.Models;
+
+public class PaymentLogVm
+{
+    public PaymentLogVm()
+    {
+        
+    }
+
+    public PaymentLogVm(PaymentOperatorLog x)
+    {
+        Id = x.Id;
+        Received = x.Received;
+        Event = x.Event;
+        Operator = x.Operator;
+        JsonEvent = x.JsonEvent;
+    }
+
+    public int Id { get; set; }
+    public DateTime Received { get; set; }
+    public string Event { get; set; }
+    public string Operator { get; set; }
+    public string JsonEvent { get; set; }
+}
---
+++ ITBees.FAS.Payments/Controllers/Models/PaymentVm.cs
@@ -0,0 +1,34 @@
+﻿using ITBees.FAS.Payments.Interfaces.Models;
+
+namespace ITBees.FAS.Payments.Controllers.Models;
+
+public class PaymentVm
+{
+    public PaymentVm()
+    {
+        
+    }
+
+    public PaymentVm(PaymentSession x)
+    {
+        PaymentSessionGuid = x.Guid;
+        Created = x.Created;
+        Finished = x.Finished;
+        Success = x.Success;
+        FinishedDate = x.FinishedDate;
+        OperatorTransactionId = x.OperatorTransactionId;
+        Email = x.CreatedBy.Email;
+        Value = x.InvoiceData.SubscriptionPlan.Value;
+    }
+
+    public decimal Value { get; set; }
+
+    public Guid PaymentSessionGuid { get; set; }
+    public DateTime Created { get; set; }
+    public bool Finished { get; set; }
+    public bool Success { get; set; }
+    public string PaymentOperator { get; set; }
+    public DateTime? FinishedDate { get; set; }
+    public string? OperatorTransactionId { get; set; }
+    public string Email { get; set; }
+}
