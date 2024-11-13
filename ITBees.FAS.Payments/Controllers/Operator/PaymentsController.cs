59f4daa04b56277c86ff42589fd7c5d337cd91bc
Jakub Zohar Rutkowski <kuba@skb.pl>
2024-11-13 16:19:26 +0100

    Format view for logs and payments

--- ITBees.FAS.Payments/Controllers/Operator/PaymentsController.cs
+++ ITBees.FAS.Payments/Controllers/Operator/PaymentsController.cs
@@ -4,6 +4,7 @@ using ITBees.Interfaces.Repository;
 using ITBees.RestfulApiControllers;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System.Text;
 
 namespace ITBees.FAS.Payments.Controllers.Operator;
 
@@ -18,8 +19,77 @@ public class PaymentsController : RestfulControllerBase<PaymentsController>
 
     [HttpGet]
     [Produces<PaginatedResult<PaymentVm>>]
-    public IActionResult Get(string? authKey, int? page, int? pageSize, string? sortColumn, SortOrder? sortOrder)
+    public IActionResult Get(bool viewAsHtml, string? authKey, int? page, int? pageSize, string? sortColumn, SortOrder? sortOrder)
     {
-        return ReturnOkResult(() => _paymentServiceInfo.Get(authKey, page, pageSize, sortColumn, sortOrder));
+        if (viewAsHtml)
+        {
+            var result = _paymentServiceInfo.Get(authKey, page, pageSize, sortColumn, sortOrder);
+
+            var html = new StringBuilder();
+
+            html.Append("<html><body>");
+
+            // Form for authKey and pagination parameters
+            html.Append("<form method='get'>");
+            html.Append("Auth Key: <input type='text' name='authKey' value='" + (authKey ?? "") + "' /> ");
+            html.Append("<input type='hidden' name='page' value='" + result.CurrentPage + "' />");
+            html.Append("<input type='hidden' name='pageSize' value='" + result.ElementsPerPage + "' />");
+            html.Append("<input type='submit' value='Submit' />");
+            html.Append("</form><br/>");
+
+            // Table of payments
+            html.Append("<table border='1'>");
+            html.Append("<thead><tr>");
+            html.Append("<th>Payment Session Guid</th>");
+            html.Append("<th>Created</th>");
+            html.Append("<th>Finished</th>");
+            html.Append("<th>Success</th>");
+            html.Append("<th>Finished Date</th>");
+            html.Append("<th>Operator Transaction Id</th>");
+            html.Append("<th>Email</th>");
+            html.Append("<th>Value</th>");
+            html.Append("</tr></thead>");
+            html.Append("<tbody>");
+
+            foreach (var item in result.Data)
+            {
+                html.Append("<tr>");
+                html.Append("<td>" + item.PaymentSessionGuid + "</td>");
+                html.Append("<td>" + item.Created + "</td>");
+                html.Append("<td>" + item.Finished + "</td>");
+                html.Append("<td>" + item.Success + "</td>");
+                html.Append("<td>" + (item.FinishedDate?.ToString() ?? "") + "</td>");
+                html.Append("<td>" + (item.OperatorTransactionId ?? "") + "</td>");
+                html.Append("<td>" + item.Email + "</td>");
+                html.Append("<td>" + item.Value + "</td>");
+                html.Append("</tr>");
+            }
+
+            html.Append("</tbody></table>");
+
+            // Pagination controls
+            html.Append("<div>");
+            html.Append($"Page {result.CurrentPage} of {result.AllPagesCount}<br/>");
+
+            if (result.CurrentPage > 1)
+            {
+                html.Append($"<a href='?authKey={authKey}&page={result.CurrentPage - 1}&pageSize={result.ElementsPerPage}'>Previous</a> ");
+            }
+
+            if (result.CurrentPage < result.AllPagesCount)
+            {
+                html.Append($"<a href='?authKey={authKey}&page={result.CurrentPage + 1}&pageSize={result.ElementsPerPage}'>Next</a>");
+            }
+
+            html.Append("</div>");
+
+            html.Append("</body></html>");
+
+            return Content(html.ToString(), "text/html");
+        }
+        else
+        {
+            return ReturnOkResult(() => _paymentServiceInfo.Get(authKey, page, pageSize, sortColumn, sortOrder));
+        }
     }
 }
--- ITBees.FAS.Payments/Controllers/Operator/PaymentsLogsController.cs
+++ ITBees.FAS.Payments/Controllers/Operator/PaymentsLogsController.cs
@@ -4,6 +4,7 @@ using ITBees.Interfaces.Repository;
 using ITBees.RestfulApiControllers;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System.Text;
 
 namespace ITBees.FAS.Payments.Controllers.Operator;
 
@@ -18,8 +19,71 @@ public class PaymentsLogsController : RestfulControllerBase<PaymentsLogsControll
 
     [HttpGet]
     [Produces<PaginatedResult<PaymentLogVm>>]
-    public IActionResult Get(string? authKey, int? page, int? pageSize, string? sortColumn, SortOrder? sortOrder)
+    public IActionResult Get(bool viewAsHtml, string? authKey, int? page, int? pageSize, string? sortColumn, SortOrder? sortOrder)
     {
-        return ReturnOkResult(() => _paymentServiceInfo.GetLogs(authKey, page, pageSize, sortColumn, sortOrder));
+        if (viewAsHtml)
+        {
+            var result = _paymentServiceInfo.GetLogs(authKey, page, pageSize, sortColumn, sortOrder);
+
+            var html = new StringBuilder();
+
+            html.Append("<html><body>");
+
+            // Form for authKey and pagination parameters
+            html.Append("<form method='get'>");
+            html.Append("Auth Key: <input type='text' name='authKey' value='" + (authKey ?? "") + "' /> ");
+            html.Append("<input type='hidden' name='page' value='" + result.CurrentPage + "' />");
+            html.Append("<input type='hidden' name='pageSize' value='" + result.ElementsPerPage + "' />");
+            html.Append("<input type='submit' value='Submit' />");
+            html.Append("</form><br/>");
+
+            // Table of payment logs
+            html.Append("<table border='1'>");
+            html.Append("<thead><tr>");
+            html.Append("<th>Id</th>");
+            html.Append("<th>Received</th>");
+            html.Append("<th>Event</th>");
+            html.Append("<th>Operator</th>");
+            html.Append("<th>Json Event</th>");
+            html.Append("</tr></thead>");
+            html.Append("<tbody>");
+
+            foreach (var item in result.Data)
+            {
+                html.Append("<tr>");
+                html.Append("<td>" + item.Id + "</td>");
+                html.Append("<td>" + item.Received + "</td>");
+                html.Append("<td>" + item.Event + "</td>");
+                html.Append("<td>" + item.Operator + "</td>");
+                html.Append("<td>" + item.JsonEvent + "</td>");
+                html.Append("</tr>");
+            }
+
+            html.Append("</tbody></table>");
+
+            // Pagination controls
+            html.Append("<div>");
+            html.Append($"Page {result.CurrentPage} of {result.AllPagesCount}<br/>");
+
+            if (result.CurrentPage > 1)
+            {
+                html.Append($"<a href='?authKey={authKey}&page={result.CurrentPage - 1}&pageSize={result.ElementsPerPage}'>Previous</a> ");
+            }
+
+            if (result.CurrentPage < result.AllPagesCount)
+            {
+                html.Append($"<a href='?authKey={authKey}&page={result.CurrentPage + 1}&pageSize={result.ElementsPerPage}'>Next</a>");
+            }
+
+            html.Append("</div>");
+
+            html.Append("</body></html>");
+
+            return Content(html.ToString(), "text/html");
+        }
+        else
+        {
+            return ReturnOkResult(() => _paymentServiceInfo.GetLogs(authKey, page, pageSize, sortColumn, sortOrder));
+        }
     }
 }
