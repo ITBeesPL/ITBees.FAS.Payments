5cd41b17574f9ef642fda58226ea48fdf149259f
Jakub Zohar Rutkowski <kuba@skb.pl>
2025-06-16 22:06:38 +0200

    Add Currency property to OctoparkContextModelSnapshot and extend TransactionResult with Stan, AuthCode, and RetrievalReferenceNumber

--- ITBees.FAS.Payments/Setup/TransactionResult.cs
+++ ITBees.FAS.Payments/Setup/TransactionResult.cs
@@ -6,4 +6,7 @@ public class TransactionResult
     public decimal Amount { get; set; }
     public string CardSuffix { get; set; }
     public string Message { get; set; }
+    public string Stan { get; set; }
+    public string AuthCode { get; set; }
+    public string RetrievalReferenceNumber { get; set; }
 }
