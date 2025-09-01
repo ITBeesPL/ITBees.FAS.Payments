5d82d5886a80eb759837b9108a9422b876cd0e7b
Kacper <fastrabbiton@gmail.com>
2025-09-01 15:02:12 +0200

    Add comments to IInvoiceDataService interface for clarity on allowUpdateIfExist parameter

--- ITBees.FAS.Payments/Interfaces/IInvoiceDataService.cs
+++ ITBees.FAS.Payments/Interfaces/IInvoiceDataService.cs
@@ -3,7 +3,9 @@
 namespace ITBees.FAS.Payments.Interfaces;
 
 public interface IInvoiceDataService
-{
+{ 
+    // allowUpdateIfExist = true - ensures that if an invoice data record already exists for the given company, it will be updated with the new data.
+    // allowUpdateIfExist = false - ensures that if an invoice data record already exists for the given company, a new record will be created instead of updating the existing one.
     InvoiceDataVm Create(InvoiceDataIm invoiceDataIm, bool allowUpdateIfExist = true);
     InvoiceDataVm Get(Guid companyGuid);
     InvoiceDataVm Update(InvoiceDataUm invoiceDataUm);
