9eeaee27f7ebbc7f217dd25b9bbb97bca222cabd
Jakub Zohar Rutkowski <kuba@skb.pl>
2025-10-28 15:27:16 +0100

    Add logo printing and enhance receipt data structure

--- ITBees.FAS.Payments/Interfaces/ICardTerminalService.cs
+++ ITBees.FAS.Payments/Interfaces/ICardTerminalService.cs
@@ -8,4 +8,9 @@ public interface ICardTerminalService
     event Action<TransactionResult> TransactionFailed;
 
     Task<TransactionResult> StartTransactionAsync(CashlessTransactionType type, decimal amount);
+    Task<TransactionResult> StartRefundAsync(decimal amount);
+    Task<TransactionResult> StartReversalAsync(decimal amount, string rrnHex12 = null);
+    Task<bool> CloseDayAsync(string messageId = "2841311", string tlvHex = null);
+    Task<bool> RequestTransactionReportAsync(string messageId = "2841211", string tlvHex = null);
+    Task SendHelloExtendedAsync(string companyName = null, string ecrVersion = null, string protocolVersionNnnmmpp = "0047511");
 }
