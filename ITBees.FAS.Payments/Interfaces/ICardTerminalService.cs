32b76ed5740eed19baef198aa8a350d7e7ec753e
Jakub Zohar Rutkowski <kuba@skb.pl>
2025-10-29 17:22:08 +0100

    Add prefixComment parameter to transaction-related methods in ICardTerminalService

--- ITBees.FAS.Payments/Interfaces/ICardTerminalService.cs
+++ ITBees.FAS.Payments/Interfaces/ICardTerminalService.cs
@@ -7,10 +7,10 @@ public interface ICardTerminalService
     event Action<TransactionResult> TransactionSucceeded;
     event Action<TransactionResult> TransactionFailed;
 
-    Task<TransactionResult> StartTransactionAsync(CashlessTransactionType type, decimal amount);
-    Task<TransactionResult> StartRefundAsync(decimal amount);
-    Task<TransactionResult> StartReversalAsync(decimal amount, string rrnHex12 = null);
-    Task<bool> CloseDayAsync(string messageId = "2841311", string tlvHex = null);
-    Task<bool> RequestTransactionReportAsync(string messageId = "2841211", string tlvHex = null);
+    Task<TransactionResult> StartTransactionAsync(CashlessTransactionType type, decimal amount, string prefixComment);
+    Task<TransactionResult> StartRefundAsync(decimal amount, string prefixComment);
+    Task<TransactionResult> StartReversalAsync(decimal amount, string prefixComment, string rrnHex12 = null);
+    Task<bool> CloseDayAsync(string prefixComment, string messageId = "2841311", string tlvHex = null);
+    Task<bool> RequestTransactionReportAsync(string prefixComment, string messageId = "2841211", string tlvHex = null);
     Task SendHelloExtendedAsync(string companyName = null, string ecrVersion = null, string protocolVersionNnnmmpp = "0047511");
 }
