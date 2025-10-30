00188a10f17aba6657e208846484a925437f7544
Jakub Zohar Rutkowski <kuba@skb.pl>
2025-10-30 18:20:47 +0100

    Add RequestPreviewCumulativeReportAsync method to ICardTerminalService

--- ITBees.FAS.Payments/Interfaces/ICardTerminalService.cs
+++ ITBees.FAS.Payments/Interfaces/ICardTerminalService.cs
@@ -13,4 +13,6 @@ public interface ICardTerminalService
     Task<bool> CloseDayAsync(string prefixComment, string messageId = "2841311", string tlvHex = null);
     Task<bool> RequestTransactionReportAsync(string prefixComment, string messageId = "2841211", string tlvHex = null);
     Task SendHelloExtendedAsync(string companyName = null, string ecrVersion = null, string protocolVersionNnnmmpp = "0047511");
+    Task<EcrReportResult> RequestPreviewCumulativeReportAsync(int bParam, int? profileId = null,
+        int timeoutMs = 20000);
 }
