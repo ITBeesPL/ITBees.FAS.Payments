31681dfc31f8e1fd67166f01de91ce461ebd2070
Jakub Zohar Rutkowski <kuba@skb.pl>
2025-11-19 13:25:07 +0100

    Refactor ICardTerminalService methods to include transactionType parameter for better transaction handling

--- ITBees.FAS.Payments/Interfaces/ICardTerminalService.cs
+++ ITBees.FAS.Payments/Interfaces/ICardTerminalService.cs
@@ -7,9 +7,9 @@ public interface ICardTerminalService
     event Action<TransactionResult> TransactionSucceeded;
     event Action<TransactionResult> TransactionFailed;
 
-    Task<TransactionResult> StartTransactionAsync(CashlessTransactionType type, decimal amount, string prefixComment);
-    Task<TransactionResult> StartRefundAsync(decimal amount, string prefixComment);
-    Task<TransactionResult> StartReversalAsync(decimal amount, string prefixComment, string rrnHex12 = null);
+    Task<TransactionResult> StartTransactionAsync(CashlessTransactionType type, decimal amount, string prefixComment, CashlessTransactionType transactionType);
+    Task<TransactionResult> StartRefundAsync(decimal amount, string prefixComment, CashlessTransactionType transactionType);
+    Task<TransactionResult> StartReversalAsync(decimal amount, string prefixComment, CashlessTransactionType transactionType, string rrnHex12 = null);
     Task<bool> CloseDayAsync(string prefixComment, string messageId = "2841311", string tlvHex = null);
     Task<bool> RequestTransactionReportAsync(string prefixComment, string messageId = "2841211", string tlvHex = null);
     Task SendHelloExtendedAsync(string companyName = null, string ecrVersion = null, string protocolVersionNnnmmpp = "0047511");
